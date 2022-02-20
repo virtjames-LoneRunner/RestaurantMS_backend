69dcdab42f3e726ad1f9b6c85a6a345c8c2ef8c8
James Lourd Virtudazo <virtjameslourd55@gmail.com>
2022-02-20 19:32:12 +0800

    Changed database from sqlite to mysql

--- main/settings.py
+++ main/settings.py
@@ -74,9 +74,17 @@ WSGI_APPLICATION = 'main.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'rma_db',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
